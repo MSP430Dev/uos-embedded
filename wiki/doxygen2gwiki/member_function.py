c536530f9c4e4098706a0d62452f58b0e9dbbeca
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2010-02-26 09:40:06 +0000

    Added doxygen2gwiki.

---
+++ wiki/doxygen2gwiki/member_function.py
@@ -0,0 +1,45 @@
+from utils import getText
+
+from templates.Function import Function
+
+class DoxygenMemberFunction:
+    def __init__(self, xml):
+        try:
+            self.refid = xml.attributes["refid"].value
+        except KeyError:
+            self.refid = xml.attributes["id"].value
+        self.name = getText(xml.getElementsByTagName("name")[0].childNodes)
+
+        l = [""]
+        [x.getLines(l) for x in convertLine(xml.getElementsByTagName("briefdescription")[0], self)]
+        self.brief = "".join(l).strip()
+
+        l = [""]
+        [x.getLines(l) for x in convertLine(xml.getElementsByTagName("detaileddescription")[0], self)]
+        self.detailed = "".join(l).strip()
+
+#	elem = xml.getElementsByTagName("type")
+	self.type = []
+#	if elem:
+#		self.type = getText(elem[0].childNodes)
+        self.params = []
+        for p in xml.getElementsByTagName("param"):
+            type = getText(p.getElementsByTagName("type")[0].childNodes)
+	    elem = p.getElementsByTagName("declname")
+	    if elem:
+		    declname = getText(elem[0].childNodes)
+		    self.params.append((type, declname))
+
+	elem = xml.getElementsByTagName("type")
+	if elem:
+		self.type = getText(elem[0].childNodes)
+
+    def __get_sig(self):
+        return self.type + " " + self.name + "(" + ", ".join(["%s %s" % x for x in self.params]) + ")"
+    sig = property(__get_sig)
+
+    def __get_doc(self):
+        return unicode(Function(searchList={"f": self}))
+    doc = property(__get_doc)
+
+from text_elements import convertLine
