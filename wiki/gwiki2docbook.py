8ba2448318e6d3ce0122ff25e13cdb1cfbec6473
serge.vakulenko <serge.vakulenko@319a1301-964c-0410-93c1-575b8fb0c566>
2010-03-22 18:18:17 +0000

    Преобразование из wiki в docbook: работают выделения и рисунки.

--- wiki/gwiki2docbook.py
+++ wiki/gwiki2docbook.py
@@ -9,47 +9,56 @@ import sys, string, getopt, re
 #
 # Default options.
 #
-language = "en"
+base_url = "http://uos-embedded.googlecode.com/svn/wiki/"
+language = ""
 verbose = 0
 section_level = 0
-para_inside = ""
+inside_para = ""
+inside_code = 0
 re_heading1 = re.compile ("=(.*)=$")
 re_heading2 = re.compile ("==(.*)==$")
 re_heading3 = re.compile ("===(.*)===$")
 re_bullets = re.compile ("  *\* (.*)$")
 re_numbering = re.compile ("  *# (.*)$")
 re_link = re.compile ("\[([^ ]*)(.*)\]")
+re_image = re.compile ("\[([^ ]*\.(png|jpg|gif))\]$")
+re_bold_left = re.compile ("\*[^ *]")
+re_bold_right = re.compile ("[^ *]\*")
+re_italic_left = re.compile ("_[^ _]")
+re_italic_right = re.compile ("[^ _]_")
+re_literal_left = re.compile ("`[^ `]")
+re_literal_right = re.compile ("[^ `]`")
 
 #
 # Start/finish a paragraph or list.
 #
 def set_para (style):
-	global para_inside
+	global inside_para
 	if style:
-		if para_inside == style:
+		if inside_para == style:
 			return
-		if para_inside:
-			print "</%s>" % para_inside
-		print "<%s>" % style
-		para_inside = style
+		if inside_para:
+			print "</%s>" % inside_para
+		print "<%s>" % style,
+		inside_para = style
 	else:
-		if not para_inside:
+		if not inside_para:
 			return
-		print "</%s>" % para_inside
-		para_inside = ""
+		print "</%s>" % inside_para
+		inside_para = ""
 
 #
 # Close paragraph or list.
 #
 def para_close():
 	print "</para>"
-	para_inside = 0
+	inside_para = 0
 
 #
 # Make a list item.
 #
 def list_item (text):
-	print "<listitem> <para>", text.strip().encode('utf-8'), "</para> </listitem>"
+	print "<listitem><para>" + text.strip().encode('utf-8') + "</para></listitem>"
 
 #
 # Start/finish a section.
@@ -68,7 +77,7 @@ def set_section (level, title=""):
 	while section_level < level:
 		section_level += 1
 		print "<sect%d>" % section_level,
-	print "<title>", title.strip().encode('utf-8'), "</title>"
+	print "<title>" + title.strip().encode('utf-8') + "</title>"
 
 #
 # Make URL links.
@@ -92,36 +101,105 @@ def parse_links (line):
 	return result
 
 #
+# Replace left and right emphasis.
+#
+def replace_emphasis (line, re_left, re_right, tag_open, tag_close):
+	match = re_left.search (line)
+	if not match:
+		return ""
+	i = match.start()
+	if i > 0 and line[i-1].isalnum():
+		return ""
+	match = re_right.search (line, i)
+	if not match:
+		return ""
+	k = match.end()
+	if k < len(line) and line[k].isalnum():
+		return ""
+	return line[:i] + tag_open + line[i+1:k-1] + tag_close + line[k:]
+
+#
+# Handle bold/italic/code emphasis.
+#
+def parse_emph (line):
+	while 1:
+		new = replace_emphasis (line, re_bold_left, re_bold_right,
+			"<emphasis role=\"bold\">", "</emphasis>")
+		if new:
+			line = new
+			continue
+		new = replace_emphasis (line, re_italic_left,
+			re_italic_right, "<emphasis>", "</emphasis>")
+		if new:
+			line = new
+			continue
+		new = replace_emphasis (line, re_literal_left,
+			re_literal_right, "<literal>", "</literal>")
+		if new:
+			line = new
+			continue
+		break
+	return line
+
+#
+# Convert &, <, > to HTML entities.
+#
+def parse_entities (line):
+	for i in reversed(range(len(line))):
+		c = line[i]
+		if c == '&':
+			line = line[:i] + "&amp;" + line[i+1:]
+		elif c == '<':
+			line = line[:i] + "&lt;" + line[i+1:]
+		elif c == '>':
+			line = line[:i] + "&gt;" + line[i+1:]
+	return line
+
+#
 # Process a single line.
 #
 def convert_line (line):
+	line = parse_links (line)
+	line = parse_emph (line)
+
 	match = re_heading3.match (line)
 	if match:
 		set_section (3, match.expand ("\\1"))
 		return
+
 	match = re_heading2.match (line)
 	if match:
 		set_section (2, match.expand ("\\1"))
 		return
+
 	match = re_heading1.match (line)
 	if match:
 		set_section (1, match.expand ("\\1"))
 		return
+
 	match = re_bullets.match (line)
 	if match:
 		set_para ("itemizedlist")
 		list_item (match.expand ("\\1"))
 		return
+
 	match = re_numbering.match (line)
 	if match:
 		set_para ("orderedlist")
 		list_item (match.expand ("\\1"))
 		return
 
-	line = parse_links (line)
 	set_para ("para")
-	print "   ", line.encode('utf-8')
+	print line.encode('utf-8')
 
+#
+# Make a reference to image file.
+#
+def put_image (url):
+	file = url
+	if url.startswith (base_url):
+		file = "file:./" + url [len(base_url):]
+	print "<graphic fileref=\"" + file + "\"></graphic>"
 
 #
 # Print usage info.
@@ -161,7 +239,7 @@ print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
 print "<!DOCTYPE article"
 print "  PUBLIC \"-//OASIS//DTD DocBook XML V4.1.2//EN\""
 print "  \"http://www.oasis-open.org/docbook/xml/4.1.2/docbookx.dtd\">"
-print "<article lang=\"\">"
+print "<article lang=\"%s\">" % language
 for filename in args:
 	# Process a single wiki page.
 	#print "--", filename, "--"
@@ -169,10 +247,35 @@ for filename in args:
 	for line in f.readlines():
 		line = unicode (line, "utf-8").rstrip()
 		if not line:
-			set_para ("")
+			if inside_code:
+				print
+			else:
+				set_para ("")
 			continue
 		if line[0] == '#':
 			continue
+		line = parse_entities (line)
+
+		# Blocks pf literal text.
+		if not inside_code and line == "{{{":
+			inside_code = 1
+			print "<screen>"
+			continue
+		if inside_code:
+			if line == "}}}":
+				inside_code = 0
+				print "</screen>"
+			else:
+				print line.encode('utf-8')
+			continue
+
+		# Inline images.
+		match = re_image.match (line)
+		if match:
+			put_image (match.expand ("\\1"))
+			continue
+
+		# Ordinary text/
 		convert_line (line)
 
 set_section (0)
